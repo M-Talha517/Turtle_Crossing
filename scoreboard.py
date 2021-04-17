fcb6482cb1527050de4a0930d53e671d51712695
M-Talha517 <muhammadtalha517@gmail.com>
2021-04-17 22:11:52 +0500

    Updated Files

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,22 @@
 from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+
+
+def save_highest_level(highest_level):
+    with open(file='score.txt', mode='w') as score:
+        score.write(highest_level)
+
+
+def load_highest_level():
+    try:
+        with open(file='score.txt', mode='r') as score:
+            highest_level = int(score.read())
+    except FileNotFoundError:
+        highest_level = 0
+        save_highest_level(str(0))
+    finally:
+        return highest_level
 
 
 class Scoreboard(Turtle):
@@ -7,6 +24,7 @@ class Scoreboard(Turtle):
         super(Scoreboard, self).__init__()
         self.level = 0
         self.penup()
+        self.highest_level = load_highest_level()
         self.hideturtle()
         self.STARTING_POSITION = starting_location
         self.update()
@@ -18,8 +36,12 @@ class Scoreboard(Turtle):
         self.write_text()
 
     def write_text(self):
-        self.write(f"Level: {self.level}", font=FONT)
+        self.write(f"Level: {self.level}    Highest Level: {self.highest_level}", font=FONT)
 
     def gameover(self):
+        if self.level > self.highest_level:
+            self.highest_level = self.level
+            save_highest_level(str(self.highest_level))
+
         self.goto(0, 0)
-        self.write(f'GAME OVER', font=FONT, align="center",)
+        self.write(f'GAME OVER', font=FONT, align="center", )
